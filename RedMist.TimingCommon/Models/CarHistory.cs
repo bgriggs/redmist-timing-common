01c6ab22e98f2b8c2b58a509e4526ab43d0384a2
Brian Griggs <brian@bigmissionmotorsports.com>
2025-02-09 19:13:50 -0700

    Initial event distribution functions.

---
+++ RedMist.TimingCommon/Models/CarHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RedMist.TimingCommon.Models;
+
+public class CarHistory
+{
+    [JsonPropertyName("cid")]
+    public string CarId { get; set; } = string.Empty;
+    [JsonPropertyName("laps")]
+    public List<Lap> Laps { get; set; } = [];
+}
