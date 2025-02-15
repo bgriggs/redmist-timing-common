e8911f19d1f9468f5a2bc82e9e8891f6bb8ab9f8
Brian Griggs <brian@bigmissionmotorsports.com>
2025-02-14 20:28:39 -0700

    Adding status payload and change delta updates and full update on client connect.

---
+++ RedMist.TimingCommon/Models/Payload.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RedMist.TimingCommon.Models;
+
+
+public class Payload
+{
+    [JsonPropertyName("e")]
+    public int EventId { get; set; }
+    [JsonPropertyName("n")]
+    public string? EventName { get; set; }
+    [JsonPropertyName("es")]
+    public EventStatus? EventStatus { get; set; }
+    [JsonPropertyName("ee")]
+    public List<EventEntry> EventEntries { get; set; } = new();
+    [JsonPropertyName("eeu")]
+    public List<EventEntry> EventEntryUpdates { get; set; } = new();
+    [JsonPropertyName("cps")]
+    public List<CarPosition> CarPositions { get; set; } = new();
+    [JsonPropertyName("cpu")]
+    public List<CarPosition> CarPositionUpdates { get; set; } = new();
+}
