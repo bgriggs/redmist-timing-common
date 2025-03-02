e33deac9d8ea20bcd88abbd73587f71845c93ef2
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-01 19:33:54 -0700

    Adding container to map car to control logs.

---
+++ RedMist.TimingCommon/Models/CarControlLogs.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RedMist.TimingCommon.Models;
+
+public class CarControlLogs
+{
+    [JsonPropertyName("cn")]
+    public string CarNumber { get; set; } = string.Empty;
+    [JsonPropertyName("entries")]
+    public List<ControlLogEntry> ControlLogEntries { get; set; } = [];
+}
