148160528ad04b85ee310f7703a7b477d8c2639e
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-12 21:20:06 -0600

    Event fields.

--- RedMist.TimingCommon/Models/Event.cs
+++ RedMist.TimingCommon/Models/Event.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json.Serialization;
+﻿using RedMist.TimingCommon.Models.Configuration;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace RedMist.TimingCommon.Models;
 
@@ -10,9 +12,15 @@ public class Event
     public string EventName { get; set; } = string.Empty;
     [JsonPropertyName("d")]
     public string EventDate { get; set; } = string.Empty;
+    public string EventUrl { get; set; } = string.Empty;
     [JsonPropertyName("s")]
     public Session[] Sessions { get; set; } = [];
     public string OrganizationName { get; set; } = string.Empty;
     public string? OrganizationWebsite { get; set; }
     public byte[]? OrganizationLogo { get; set; }
+    public EventSchedule? Schedule { get; set; }
+    public string TrackName { get; set; } = string.Empty;
+    public string CourseConfiguration { get; set; } = string.Empty;
+    public string Distance { get; set; } = string.Empty;
+    public BroadcasterConfig? Broadcast { get; set; }
 }
