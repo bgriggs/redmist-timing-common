b5c00e2f4f2f208ce6b60cbacefcab1f9aaa0857
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-22 16:46:18 -0600

    Adding x2 models.

---
+++ RedMist.TimingCommon/Models/X2/Loop.cs
@@ -0,0 +1,19 @@
+﻿namespace RedMist.TimingCommon.Models.X2;
+
+public class Loop
+{
+    public uint Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public double Latitude0 { get; set; }
+    public double Longitude0 { get; set; }
+    public double Latitude1 { get; set; }
+    public double Longitude1 { get; set; }
+    public uint Order { get; set; }
+    public bool IsInPit { get; set; }
+    public bool IsOnline { get; set; }
+    public bool HasActivity { get; set; }
+    public bool IsSyncOk { get; set; }
+    public bool HasDeviceWarnings { get; set; }
+    public bool HasDeviceErrors { get; set; }
+}
---
+++ RedMist.TimingCommon/Models/X2/Passing.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RedMist.TimingCommon.Models.X2;
+
+public class Passing
+{
+    [JsonPropertyName("i")]
+    public uint Id { get; set; }
+    [JsonPropertyName("l")]
+    public uint LoopId { get; set; }
+    [JsonPropertyName("t")]
+    public uint TransponderId { get; set; }
+    [JsonPropertyName("ts")]
+    public uint TransponderShortId { get; set; }
+    [JsonPropertyName("h")]
+    public ushort Hits { get; set; }
+    [JsonPropertyName("tl")]
+    public DateTime TimestampLocal { get; set; }
+    [JsonPropertyName("tu")]
+    public DateTime TimestampUtc { get; set; }
+    [JsonPropertyName("p")]
+    public bool IsInPit { get; set; }
+    [JsonPropertyName("r")]
+    public bool IsResend { get; set; }
+}
