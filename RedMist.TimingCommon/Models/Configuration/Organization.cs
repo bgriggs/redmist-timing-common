0ef03eb4fd57f66f1eb7e115869b8e9e24cc66ab
Brian Griggs <brian.griggs@gmail.com>
2025-06-29 15:09:07 -0600

    Adding mutliloop fields.

--- RedMist.TimingCommon/Models/Configuration/Organization.cs
+++ RedMist.TimingCommon/Models/Configuration/Organization.cs
@@ -33,4 +33,13 @@ public class Organization
     public OrbitsConfiguration Orbits { get; set; } = new OrbitsConfiguration();
 
     public X2Configuration X2 { get; set; } = new X2Configuration();
+
+    [MaxLength(40)]
+    public string? RMonitorIp { get; set; }
+    public int RMonitorPort { get; set; } = 50000;
+    [MaxLength(40)]
+    public string? MultiloopIp { get; set; }
+    public int MultiloopPort { get; set; } = 50004;
+    [MaxLength(512)]
+    public string? OrbitsLogsPath { get; set; }
 }
