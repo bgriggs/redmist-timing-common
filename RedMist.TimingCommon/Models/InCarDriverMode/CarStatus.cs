794e55cc13c4984e3b912269ef962be63a7579e3
Brian Griggs <brian@bigmissionmotorsports.com>
2025-09-29 16:50:15 -0600

    Adding MessagePackObject to in-car classes.

--- RedMist.TimingCommon/Models/InCarDriverMode/CarStatus.cs
+++ RedMist.TimingCommon/Models/InCarDriverMode/CarStatus.cs
@@ -1,60 +1,72 @@
-﻿using System.Text.Json.Serialization;
+﻿using MessagePack;
+using System.Text.Json.Serialization;
 
 namespace RedMist.TimingCommon.Models.InCarDriverMode;
 
 /// <summary>
 /// Represents the current status of a car, including its position and lap metadata like gap and diff.
 /// </summary>
+[MessagePackObject]
 public class CarStatus
 {
     /// <summary>
     /// Car number.
     /// </summary>
     [JsonPropertyName("n")]
+    [MessagePack.Key(0)] 
     public string Number { get; set; } = string.Empty;
     /// <summary>
     /// Gets or sets the class identifier.
     /// </summary>
     [JsonPropertyName("c")]
+    [MessagePack.Key(1)]
     public string Class { get; set; } = string.Empty;
     /// <summary>
     /// Gets or sets the name of the team.
     /// </summary>
     [JsonPropertyName("t")]
+    [MessagePack.Key(2)]
     public string Team { get; set; } = string.Empty;
     /// <summary>
     /// Gets or sets the time of the last completed lap, represented as a string mm:ss.ttt.
     /// </summary>
     [JsonPropertyName("l")]
+    [MessagePack.Key(3)]
     public string LastLap { get; set; } = string.Empty;
     /// <summary>
     /// Gets or sets the gain or loss of the last lap.
     /// </summary>
     [JsonPropertyName("gl")]
+    [MessagePack.Key(4)]
     public string GainLoss { get; set; } = string.Empty;
     /// <summary>
     /// Gets or sets the gap to the next car.
     /// </summary>
     [JsonPropertyName("g")]
+    [MessagePack.Key(5)]
     public string Gap { get; set; } = string.Empty;
     /// <summary>
     /// Gets or sets the type of the car if available from competitor metadata.
     /// </summary>
     [JsonPropertyName("ct")]
+    [MessagePack.Key(6)]
     public string CarType { get; set; } = string.Empty;
     /// <summary>
     /// Gets or sets the name of the driver if available.
     /// </summary>
     [JsonPropertyName("d")]
+    [MessagePack.Key(7)]
     public string Driver { get; set; } = string.Empty;
     /// <summary>
     /// Gets or sets the unique identifier for the transponder.
     /// </summary>
     [JsonPropertyName("id")]
+    [MessagePack.Key(8)]
     public uint TransponderId { get; set; }
     /// <summary>
     /// Gets or sets the URL of the car image if available.
     /// </summary>
     [JsonPropertyName("i")]
+    [MessagePack.Key(9)]
     public string CarImageUrl { get; set; } = string.Empty;
 }
--- RedMist.TimingCommon/Models/InCarDriverMode/InCarPayload.cs
+++ RedMist.TimingCommon/Models/InCarDriverMode/InCarPayload.cs
@@ -1,20 +1,27 @@
-﻿using System.Text.Json.Serialization;
+﻿using MessagePack;
+using System.Text.Json.Serialization;
 
 namespace RedMist.TimingCommon.Models.InCarDriverMode;
 
 /// <summary>
 /// Update for in-car driver mode payload.
 /// </summary>
+[MessagePackObject]
 public class InCarPayload
 {
     [JsonPropertyName("n")]
+    [MessagePack.Key(0)]
     public string CarNumber { get; set; } = string.Empty;
-    [JsonPropertyName("p")] 
+    [JsonPropertyName("p")]
+    [MessagePack.Key(1)]
     public string PositionInClass { get; set; } = string.Empty;
-    [JsonPropertyName("o")] 
+    [JsonPropertyName("o")]
+    [MessagePack.Key(2)]
     public string PositionOverall { get; set; } = string.Empty;
     [JsonPropertyName("f")]
+    [MessagePack.Key(3)]
     public Flags Flag { get; set; } = Flags.Unknown;
     [JsonPropertyName("c")]
+    [MessagePack.Key(4)]
     public List<CarStatus?> Cars { get; set; } = [];
 }
