eabd96a20911e660eb047bcff2c0bfa858215fb5
Brian Griggs <brian@bigmissionmotorsports.com>
2026-01-21 18:26:21 -0700

    Improving enum documentation

--- RedMist.TimingCommon/Models/Flags.cs
+++ RedMist.TimingCommon/Models/Flags.cs
@@ -2,14 +2,42 @@
 
 public enum Flags
 {
+    /// <summary>
+    /// Value: 0
+    /// </summary>
     Unknown,
+    /// <summary>
+    /// Value: 1
+    /// </summary>
     Green,
+    /// <summary>
+    /// Value: 2
+    /// </summary>
     Yellow,
+    /// <summary>
+    /// Value: 3
+    /// </summary>
     Red,
+    /// <summary>
+    /// Value: 4
+    /// </summary>
     White,
+    /// <summary>
+    /// Value: 5
+    /// </summary>
     Checkered,
+    /// <summary>
+    /// Value: 6
+    /// </summary>
     Black,
-    Purple35
+    /// <summary>
+    /// Purple 35mph.Value: 7
+    /// </summary>
+    Purple35,
+    /// <summary>
+    /// Purple 60kph. Value: 8
+    /// </summary>
+    Purple60
 }
 
 public static class FlagsExtensions
@@ -25,6 +53,7 @@ public static class FlagsExtensions
             "Finish" => Flags.Checkered,
             "Black" => Flags.Black,
             "Purple" => Flags.Purple35,
+            "Purple60" => Flags.Purple60,
             _ => Flags.Unknown,
         };
     }
--- RedMist.TimingCommon/Models/InCarVideo/VideoDestinationType.cs
+++ RedMist.TimingCommon/Models/InCarVideo/VideoDestinationType.cs
@@ -5,9 +5,24 @@
 /// </summary>
 public enum VideoDestinationType
 {
+    /// <summary>
+    /// Value: 0
+    /// </summary>
     None,
+    /// <summary>
+    /// Value: 1
+    /// </summary>
     Youtube,
+    /// <summary>
+    /// Value: 2
+    /// </summary>
     Facebook,
+    /// <summary>
+    /// Value: 3
+    /// </summary>
     Vimeo,
+    /// <summary>
+    /// Value: 4
+    /// </summary>
     DirectSrt
 }
