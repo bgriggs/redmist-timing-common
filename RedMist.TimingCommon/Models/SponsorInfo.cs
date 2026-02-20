93526fe43fcb8b29f476d42a98ce8a94bb8b5a04
Brian Griggs <brian@bigmissionmotorsports.com>
2026-02-19 18:35:40 -0700

    Adding SponsorInfo model.

---
+++ RedMist.TimingCommon/Models/SponsorInfo.cs
@@ -0,0 +1,32 @@
+﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
+using KeyAttribute = MessagePack.KeyAttribute;
+
+namespace RedMist.TimingCommon.Models;
+
+[MessagePackObject]
+public class SponsorInfo
+{
+    [Key(0)]
+    public int Id { get; set; }
+    
+    [Key(1)]
+    [MaxLength(100)]
+    public required string Name { get; set; }
+
+    [Key(2)]
+    [MaxLength(1024)]
+    public required string ImageUrl { get; set; }
+
+    [Key(3)]
+    [MaxLength(1024)]
+    public required string TargetUrl { get; set; }
+
+    [Key(4)]
+    [MaxLength(200)]
+    public string? AltText { get; set; } = string.Empty;
+    [Key(5)]
+    public int DisplayDurationMs { get; set; }
+    [Key(6)]
+    public int DisplayPriority { get; set; }
+}
